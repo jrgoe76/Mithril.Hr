0bcdae6ff8864ff4cc35f9e6d6327cc7fa72adfa
Jorge Rodriguez <jorge@buildinglink.com>
2024-01-15 12:10:16 -0500

    Add Positions and tests

---
+++ src/Mithril.Hr/Domain/Positions/IGetPositionByPositionCodeQuery.cs
@@ -0,0 +1,6 @@
+﻿namespace Mithril.Hr.Domain.Positions;
+
+public interface IGetPositionByPositionCodeQuery
+{
+    public Task<Position> Get(string positionCode);
+}
---
+++ src/Mithril.Hr/Domain/Positions/Position.cs
@@ -0,0 +1,32 @@
+﻿namespace Mithril.Hr.Domain.Positions;
+
+public record Position
+{
+    public string PositionCode { get; init; }
+    public string Name { get; private set; }
+    public byte SubordinatesLimit { get; private set; }
+
+    public Position(
+        string code,
+        string name,
+        byte subordinatesLimit)
+    {
+        const string errorMessage = $"The {nameof(Position)} is invalid";
+
+        if (string.IsNullOrEmpty(code))
+        {
+            throw new ArgumentException(errorMessage, nameof(code));
+        }
+        if (string.IsNullOrEmpty(name))
+        {
+            throw new ArgumentException(errorMessage, nameof(name));
+        }
+
+        PositionCode = code;
+        Name = name;
+        SubordinatesLimit = subordinatesLimit;
+    }
+
+    public override string ToString()
+        => Name;
+}
