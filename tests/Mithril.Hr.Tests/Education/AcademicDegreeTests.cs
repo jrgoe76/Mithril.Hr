6e180fa68c579e7c87f36479a6e0c3ffc6517f56
Jorge Rodriguez <jorge@buildinglink.com>
2024-01-15 12:06:46 -0500

    Add Education and tests

---
+++ tests/Mithril.Hr.Tests/Education/AcademicDegreeTests.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using Mithril.Hr.Domain.Education;
+using Xunit;
+
+namespace Mithril.Hr.Tests.Education;
+
+public sealed class AcademicDegreeTests
+{
+    [Fact]
+    public void ThrowsArgumentException()
+    {
+        ((Func<AcademicDegree>)(() => new AcademicDegree("XXX")))
+            .Should().Throw<ArgumentException>();
+    }
+
+    [Fact]
+    public void ReturnsRepresentation()
+    {
+        new AcademicDegree(nameof(AcademicDegree.Values.Associate)).ToString()
+            .Should().Be(nameof(AcademicDegree.Values.Associate));
+        new AcademicDegree(nameof(AcademicDegree.Values.Bachelor)).ToString()
+            .Should().Be(nameof(AcademicDegree.Values.Bachelor));
+        new AcademicDegree(nameof(AcademicDegree.Values.Master)).ToString()
+            .Should().Be(nameof(AcademicDegree.Values.Master));
+        new AcademicDegree(nameof(AcademicDegree.Values.PhD)).ToString()
+            .Should().Be(nameof(AcademicDegree.Values.PhD));
+    }
+}
