98428fd3802682f330fcec88ed8ae065bd1f8727
Jorge Rodriguez <jorge@buildinglink.com>
2024-01-15 12:04:46 -0500

    Add Demographics and tests

---
+++ src/Mithril.Hr/Domain/Demographics/Address.cs
@@ -0,0 +1,61 @@
+﻿namespace Mithril.Hr.Domain.Demographics;
+
+public record Address
+{
+    public string AddressLine1 { get; init; }
+    public string? AddressLine2 { get; init; }
+    public string City { get; init; }
+    public string State { get; init; }
+    public string Zipcode { get; init; }
+
+    public Address(
+        string addressLine1,
+        string? addressLine2,
+        string city,
+        string state,
+        string zipcode)
+    {
+        const string errorMessage = $"The {nameof(Address)} is invalid";
+
+        if (string.IsNullOrEmpty(addressLine1))
+        {
+            throw new ArgumentException(errorMessage, nameof(addressLine1));
+        }
+        if (string.IsNullOrEmpty(city))
+        {
+            throw new ArgumentException(errorMessage, nameof(city));
+        }
+        if (string.IsNullOrEmpty(state))
+        {
+            throw new ArgumentException(errorMessage, nameof(state));
+        }
+        if (string.IsNullOrEmpty(zipcode))
+        {
+            throw new ArgumentException(errorMessage, nameof(zipcode));
+        }
+
+        AddressLine1 = addressLine1;
+        AddressLine2 = addressLine2;
+        City = city;
+        State = state;
+        Zipcode = zipcode;
+    }
+
+    public Address(
+        string addressLine1,
+        string city,
+        string state,
+        string zipcode)
+        : this(addressLine1, null, city, state, zipcode)
+    {
+    }
+
+    public override string ToString()
+    {
+        var addressLine2 = !string.IsNullOrEmpty(AddressLine2)
+            ? " " + AddressLine2
+            : string.Empty;
+
+        return $"{AddressLine1}{addressLine2}, {City}, {State} {Zipcode}";
+    }
+}
---
+++ tests/Mithril.Hr.Tests/Domain/Demographics/AddressTests.cs
@@ -0,0 +1,58 @@
+﻿using FluentAssertions;
+using Mithril.Hr.Domain.Demographics;
+using Xunit;
+
+namespace Mithril.Hr.Tests.Domain.Demographics;
+
+public sealed class AddressTests
+{
+    [Fact]
+    public void ThrowsArgumentExceptionCausedByAddressLine1()
+    {
+        ((Func<Address>)(() => new Address(null!, "Tampa", "FL", "12345")))
+            .Should().Throw<ArgumentException>();
+        ((Func<Address>)(() => new Address(string.Empty, "Tampa", "FL", "12345")))
+            .Should().Throw<ArgumentException>();
+    }
+
+    [Fact]
+    public void ThrowsArgumentExceptionCausedByCity()
+    {
+        ((Func<Address>)(() => new Address("123 Main St", null!, "FL", "12345")))
+            .Should().Throw<ArgumentException>();
+        ((Func<Address>)(() => new Address("123 Main St", string.Empty, "FL", "12345")))
+            .Should().Throw<ArgumentException>();
+    }
+
+    [Fact]
+    public void ThrowsArgumentExceptionCausedByState()
+    {
+        ((Func<Address>)(() => new Address("123 Main St", "Tampa", null!, "12345")))
+            .Should().Throw<ArgumentException>();
+        ((Func<Address>)(() => new Address("123 Main St", "Tampa", string.Empty, "12345")))
+            .Should().Throw<ArgumentException>();
+    }
+
+    [Fact]
+    public void ThrowsArgumentExceptionCausedByZipcode()
+    {
+        ((Func<Address>)(() => new Address("123 Main St", "Tampa", "FL", null!)))
+            .Should().Throw<ArgumentException>();
+        ((Func<Address>)(() => new Address("123 Main St", "Tampa", "FL", string.Empty)))
+            .Should().Throw<ArgumentException>();
+    }
+
+    [Fact]
+    public void ReturnsRepresentationWithoutAddressLine2()
+    {
+        new Address("123 Main St", "Tampa", "FL", "12345").ToString()
+            .Should().Be("123 Main St, Tampa, FL 12345");
+    }
+
+    [Fact]
+    public void ReturnsRepresentation()
+    {
+        new Address("123 Main St", "Apt B", "Tampa", "FL", "12345").ToString()
+            .Should().Be("123 Main St Apt B, Tampa, FL 12345");
+    }
+}
