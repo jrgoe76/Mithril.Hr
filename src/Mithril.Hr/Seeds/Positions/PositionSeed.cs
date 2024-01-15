2b8042ad2584df836601299e6a5ecbbe3b761545
Jorge Rodriguez <jorge@buildinglink.com>
2024-01-15 12:52:31 -0500

    Adds Employees and tests

---
+++ src/Mithril.Hr/Seeds/Positions/PositionSeed.cs
@@ -0,0 +1,17 @@
+﻿using Mithril.Hr.Domain.Positions;
+
+namespace Mithril.Hr.Seeds.Positions;
+
+public static class PositionSeed
+{
+    public static Position ChiefExecutiveOfficer = new ("CEO", "Chief Executive Officer", 5);
+
+    public static Position ChiefOperatingOfficer = new ("COO", "Chief Operating Officer", 5);
+    public static Position StoreManager = new ("STR_MGR", "Store Manager", 10);
+    public static Position SectionLeader = new ("SEC_LED", "Section Leader", 5);
+    public static Position StockAssistant = new ("STK_AST", "Stock Assistant", 0);
+
+    public static Position ChiefFinancialOfficer = new ("CFO", "Chief Financial Officer", 5);
+    public static Position AccountingManager = new ("ACT_MGR", "Accounting Manager", 10);
+    public static Position Accountant = new ("ACT", "Accountant", 0);
+}
---
+++ tests/Mithril.Hr.Tests/Domain/Employees/EmployeeTests.cs
@@ -0,0 +1,76 @@
+﻿using FluentAssertions;
+using Mithril.Hr.Domain.Demographics;
+using Mithril.Hr.Domain.Education;
+using Mithril.Hr.Domain.Employees;
+using Mithril.Hr.Seeds.Demographics;
+using Mithril.Hr.Seeds.Employees;
+using Mithril.Hr.Seeds.Positions;
+using Xunit;
+
+namespace Mithril.Hr.Tests.Domain.Employees;
+
+public sealed class EmployeeTests
+{
+    private readonly PersonName _name = PersonNameSeed.DianaKing;
+    private readonly Gender _gender = Gender.Female;
+    private readonly Email _email = EmailSeed.DianaKingAtAol;
+    private readonly Address _address = AddressSeed.MadisonAve;
+    private readonly AcademicDegree _degree = AcademicDegree.PhD;
+
+    [Fact]
+    public void ThrowsArgumentException()
+    {
+        ((Func<Employee>)(() => new Employee(Guid.Empty, _name, _gender, _email, _address, _degree)))
+            .Should().Throw<ArgumentException>();
+        ((Func<Employee>)(() => new Employee(Guid.NewGuid(), null!, _gender, _email, _address, _degree)))
+            .Should().Throw<ArgumentException>();
+        ((Func<Employee>)(() => new Employee(Guid.NewGuid(), _name, null!, _email, _address, _degree)))
+            .Should().Throw<ArgumentException>();
+        ((Func<Employee>)(() => new Employee(Guid.NewGuid(), _name, _gender, null!, _address, _degree)))
+            .Should().Throw<ArgumentException>();
+        ((Func<Employee>)(() => new Employee(Guid.NewGuid(), _name, _gender, _email, null!, _degree)))
+            .Should().Throw<ArgumentException>();
+        ((Func<Employee>)(() => new Employee(Guid.NewGuid(), _name, _gender, _email, _address, null!)))
+            .Should().Throw<ArgumentException>();
+    }
+
+    [Fact]
+    public void UpdatesEmployee()
+    {
+        var employee = new Employee(Guid.NewGuid(), _name, _gender, _email, _address, _degree);
+
+        var updatedName = _name with { FirstName = "updated" };
+        var updatedGender = Gender.Female;
+        var updatedEmail = new Email("updated@gmail.com");
+        var updatedAddress = _address with { Zipcode = "98765" };
+        var updatedDegree = AcademicDegree.Bachelor;
+
+        employee.Update(updatedName, updatedGender, updatedEmail, updatedAddress, updatedDegree);
+
+        employee.EmployeeId
+            .Should().Be(employee.EmployeeId);
+        employee.Name
+            .Should().Be(updatedName);
+        employee.Gender
+            .Should().Be(updatedGender);
+        employee.Email
+            .Should().Be(updatedEmail);
+        employee.Address
+            .Should().Be(updatedAddress);
+        employee.Degree
+            .Should().Be(updatedDegree);
+    }
+
+    [Fact]
+    public void AssignsPosition()
+    {
+        var employee = new Employee(Guid.NewGuid(), _name, _gender, _email, _address, _degree);
+
+        employee.AssignPosition(PositionSeed.ChiefFinancialOfficer, EmployeeSeed.LiamHill);
+
+        employee.Position
+            .Should().Be(PositionSeed.ChiefFinancialOfficer);
+        employee.SupervisorId
+            .Should().Be(EmployeeSeed.LiamHill.SupervisorId);
+    }
+}
